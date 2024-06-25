24c4770bd02e23285bb18f123ec7c03f2e21fe93
veronikanos <vveronikanika@gmail.com>
2024-06-25 21:23:48 +0300

    test

--- script.js
+++ script.js
@@ -16,91 +16,56 @@ function changeHeroImage() {
 }
 
 function rotateLogo() {
-  var image = document.getElementById("rotatedLogo");
-  var currentRotation = 0; // Current angle of rotation
-  var rotating = false; // State to manage rotation direction
+  // // YOUTUBE VIDEO PLAYS
+  // var cover = document.getElementById("video-cover");
+  // var video = document.getElementById("video");
+  // cover.addEventListener("click", function () {
+  //   cover.style.display = "none";
+  //   video.style.display = "block";
+  //   video.src += "?autoplay=1"; // Automatically start the video
+  // });
 
-  setInterval(function () {
-    if (!rotating) {
-      // Rotate to 90 degrees
-      currentRotation = 90;
-      image.style.transition = "transform 0.3s ease-in-out";
-    } else {
-      // Rotate back to 0 degrees
-      currentRotation = 0;
-      image.style.transition = "transform 0.3s ease-in-out";
-    }
-    // Apply the rotation
-    image.style.transform =
-      "translate(-50%, -50%) rotate(" + currentRotation + "deg)";
-    rotating = !rotating; // Toggle rotation state
-  }, 3000); // Change rotation every 3.3 seconds to include pause time
+  const compass = document.getElementById("rotatedLogo");
+  let currentRotation = 0;
+  let rotating = false;
 
-  // YOUTUBE VIDEO PLAYS
-  var cover = document.getElementById("video-cover");
-  var video = document.getElementById("video");
-
-  cover.addEventListener("click", function () {
-    cover.style.display = "none";
-    video.style.display = "block";
-    video.src += "?autoplay=1"; // Automatically start the video
-  });
+  setInterval(() => {
+    rotating = !rotating;
+    currentRotation = rotating ? 90 : 0;
+    compass.style.transition = "transform 0.3s ease-in-out";
+    compass.style.transform = `translate(-50%, -50%) rotate(${currentRotation}deg)`;
+  }, 3300);
 }
 
 // SLIDER HERO
 
-function initializeSwiper() {
+function initializeHeroSwiper() {
   if (window.innerWidth < 1440) {
-    const swiper = new Swiper(".swiper", {
-      // Optional parameters
-      // direction: "vertical",
+    new Swiper(".swiper-hero", {
       loop: true,
       autoplay: {
         delay: 2000,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
       },
-      // If we need pagination
-      // pagination: {
-      //   el: ".swiper-pagination",
-      // },
-
-      // Navigation arrows
-      // navigation: {
-      //   nextEl: ".swiper-button-next",
-      //   prevEl: ".swiper-button-prev",
-      // },
-
-      // And if we need scrollbar
-      // scrollbar: {
-      //   el: ".swiper-scrollbar",
-      // },
     });
   }
+}
 
-  // const swiperTestimonials = new Swiper(".swiper-testimonials", {
-  // Optional parameters
-  // direction: "vertical",
-  // loop: true,
-  // autoplay: {
-  //   delay: 2000,
-  //   disableOnInteraction: false,
-  //   pauseOnMouseEnter: true,
-  // },
-  // If we need pagination
-  // pagination: {
-  //   el: ".swiper-pagination",
-  // },
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: ".swiper-scrollbar",
-  // },
-  // });
+function initializeTestimonialsSwiper() {
+  new Swiper(".swiper-testimonials", {
+    direction: "vertical",
+    loop: true,
+    // autoplay: {
+    //   delay: 2000,
+    //   disableOnInteraction: false,
+    //   pauseOnMouseEnter: true,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
 
 window.addEventListener("resize", function () {
@@ -111,9 +76,9 @@ window.addEventListener("resize", function () {
   }
 });
 
-document.addEventListener(
-  "DOMContentLoaded",
+document.addEventListener("DOMContentLoaded", () => {
   changeHeroImage(),
-  rotateLogo(),
-  initializeSwiper()
-);
+    rotateLogo(),
+    initializeHeroSwiper(),
+    initializeTestimonialsSwiper();
+});
